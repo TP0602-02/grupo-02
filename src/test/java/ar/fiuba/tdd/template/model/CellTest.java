42657a8345673a10ed02a1a732edd0ead25278e7
Nicolas Dubiansky <nicodubi111@gmail.com>
2016-10-07 22:50:34 -0300

    add cell tests

--- src/test/java/ar/fiuba/tdd/template/model/CellTest.java
+++ src/test/java/ar/fiuba/tdd/template/model/CellTest.java
@@ -13,53 +13,49 @@ public class CellTest {
     private CellFactory cellFactory;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         cellFactory = new CellFactory();
     }
 
     @Test
-    public void createCellSingleValue()
-    {
-        Assert.assertTrue(cellFactory.createCell(CellFactory.CELL_SINGLE_VALUE,1,1) instanceof CellSingleValue);
+    public void createCellSingleValue() {
+        Assert.assertTrue(cellFactory.createCell(CellFactory.CELL_SINGLE_VALUE, 1, 1) instanceof CellSingleValue);
     }
 
     @Test
-    public void cellMultipleValueIsTheDefaultCellToCreate()
-    {
-        Assert.assertTrue(cellFactory.createCell("djksnld",1,1) instanceof CellMultipleValue);
-        Assert.assertTrue(cellFactory.createCell("",1,1) instanceof CellMultipleValue);
+    public void cellMultipleValueIsTheDefaultCellToCreate() {
+        Assert.assertTrue(cellFactory.createCell("djksnld", 1, 1) instanceof CellMultipleValue);
+        Assert.assertTrue(cellFactory.createCell("", 1, 1) instanceof CellMultipleValue);
     }
 
 
     @Test
-    public void cellSingleValueAllowOnlyOneValueToAdd()
-    {
-        Cell cell = cellFactory.createCell(CellFactory.CELL_SINGLE_VALUE,1,1);
+    public void cellSingleValueAllowOnlyOneValueToAdd() {
+        Cell cell = cellFactory.createCell(CellFactory.CELL_SINGLE_VALUE, 1, 1);
         int firstValueToAdd = 3;
         int secondValueToAdd = 14;
         CellContent cellContent1 = new ValueContent(firstValueToAdd);
         CellContent cellContent2 = new ValueContent(secondValueToAdd);
         cell.setContent(cellContent1);
         cell.setContent(cellContent2);
-        Assert.assertEquals(cell.getContents().size(),1);
-        Assert.assertEquals(cell.getContents().get(0).getValue(),secondValueToAdd);
+        Assert.assertEquals(cell.getContents().size(), 1);
+        Assert.assertEquals(cell.getContents().get(0).getValue(), secondValueToAdd);
 
     }
 
 
     @Test
-    public void cellMultipleValueAllowMultipleValueToAdd()
-    {
-        Cell cell = cellFactory.createCell("",1,1);
+    public void cellMultipleValueAllowMultipleValueToAdd() {
+        Cell cell = cellFactory.createCell("", 1, 1);
         int firstValueToAdd = 3;
         int secondValueToAdd = 14;
         CellContent cellContent1 = new ValueContent(firstValueToAdd);
         CellContent cellContent2 = new ValueContent(secondValueToAdd);
         cell.setContent(cellContent1);
         cell.setContent(cellContent2);
-        Assert.assertEquals(cell.getContents().size(),2);
-        Assert.assertEquals(cell.getContents().get(0).getValue(),firstValueToAdd);
-        Assert.assertEquals(cell.getContents().get(1).getValue(),secondValueToAdd);
+        Assert.assertEquals(cell.getContents().size(), 2);
+        Assert.assertEquals(cell.getContents().get(0).getValue(), firstValueToAdd);
+        Assert.assertEquals(cell.getContents().get(1).getValue(), secondValueToAdd);
 
     }
 }

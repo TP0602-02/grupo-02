664cf88fbdd6eebeba98165eaf496b5562e80259
matikamien_93@hotmail.com <matikamien_93@hotmail.com>
2016-10-08 01:22:53 -0300

    Implements no repeat rule

--- src/main/java/ar/fiuba/tdd/template/rules/NoRepeatValueValidationRule.java
+++ src/main/java/ar/fiuba/tdd/template/rules/NoRepeatValueValidationRule.java
@@ -1,16 +1,20 @@
 package ar.fiuba.tdd.template.rules;
 
-import ar.fiuba.tdd.template.board.Board;
 import ar.fiuba.tdd.template.board.Region;
 import ar.fiuba.tdd.template.board.cell.model.Cell;
-import ar.fiuba.tdd.template.board.cell.model.CellContent;
-
-import java.util.ArrayList;
 
 public class NoRepeatValueValidationRule extends GenericRule {
 
     @Override
     public boolean validateRegion(Region region, Cell cell, int numberToAdd) {
+        for (Cell actualCell : region.getCells()) {
+            //Revisar el get(0) si hay casos donde pueda haber más valores.
+            //int actualCellValue = actualCell.getContents().get(0).getValue().getValueAsInt(); ESTE ESTÁ BIEN.
+            int actualCellValue = (int)actualCell.getContents().get(0).getValue();//Este se va a borrar, cuando se cambie T a GenericValue.
+            if (actualCell != cell && actualCellValue == numberToAdd) {
+                return false;
+            }
+        }
         return true;
     }
 }

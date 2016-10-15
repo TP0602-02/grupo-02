66200407dda4e6374ae0e1aee808b16b4cb89d14
alealazraqui <alejandro.alazraqui@gmail.com>
2016-10-15 19:10:52 -0300

    agrego test a rules regiontotalborder y regiontotalconection

---
+++ src/test/java/ar/fiuba/tdd/template/model/RegionTotalBorderRuleTest.java
@@ -0,0 +1,67 @@
+package ar.fiuba.tdd.template.model;
+
+import ar.fiuba.tdd.template.board.Board;
+import ar.fiuba.tdd.template.board.Region;
+import ar.fiuba.tdd.template.board.cell.model.Cell;
+import ar.fiuba.tdd.template.board.cell.model.ValueContent;
+import ar.fiuba.tdd.template.rules.RegionTotalBorderRule;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alazraqui on 15/10/2016.
+ */
+public class RegionTotalBorderRuleTest {
+    private Board board;
+    private Region region;
+    private RegionTotalBorderRule rule;
+
+    @Before
+    public void setUp() {
+        this.board = new Board(4, 4, "");
+        ArrayList<Cell> cell = new ArrayList<Cell>();
+        cell.add(this.board.getCell(0, 0));
+        this.region = new Region(cell);
+        region.setTotal(2);
+        this.board.addRegion(region);
+        this.rule = new RegionTotalBorderRule();
+    }
+
+    @Test
+    public void checkInEmptyBoard_ReturnFalse() {
+        Assert.assertTrue(!this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkInCellWithOneValue_ReturnFalse() {
+        this.board.getCell(0, 0).setContent(new ValueContent(3));
+        Assert.assertTrue(!this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkInCellWithTwoValues_ReturnTrue() {
+        this.board.getCell(0, 0).setContent(new ValueContent(3));
+        this.board.getCell(0, 0).setContent(new ValueContent(2));
+        Assert.assertTrue(this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkInCellWithThreeValues_ReturnFalse() {
+        this.board.getCell(0, 0).setContent(new ValueContent(3));
+        this.board.getCell(0, 0).setContent(new ValueContent(2));
+        this.board.getCell(0, 0).setContent(new ValueContent(1));
+        Assert.assertTrue(!this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkInCellWithThreeValuesInNoRestrictionRegion_ReturnTrue() {
+        this.board.getCell(0, 0).setContent(new ValueContent(3));
+        this.board.getCell(0, 0).setContent(new ValueContent(2));
+        this.board.getCell(0, 0).setContent(new ValueContent(1));
+        this.region.setTotal(-1);
+        Assert.assertTrue(this.rule.validate(this.board));
+    }
+}
---
+++ src/test/java/ar/fiuba/tdd/template/model/RegionTotalConectionRuleTest.java
@@ -0,0 +1,80 @@
+package ar.fiuba.tdd.template.model;
+
+import ar.fiuba.tdd.template.board.Board;
+import ar.fiuba.tdd.template.board.Region;
+import ar.fiuba.tdd.template.board.cell.model.Cell;
+import ar.fiuba.tdd.template.board.cell.model.ValueContent;
+import ar.fiuba.tdd.template.rules.RegionTotalBorderRule;
+import ar.fiuba.tdd.template.rules.RegionTotalConnectionRule;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alazraqui on 15/10/2016.
+ */
+public class RegionTotalConectionRuleTest {
+    private Board board;
+    private Region region;
+    private RegionTotalConnectionRule rule;
+
+    @Before
+    public void setUp() {
+        this.board = new Board(4, 4, "");
+        ArrayList<Cell> cells = new ArrayList<Cell>();
+        cells.add(this.board.getCell(0, 0));
+        cells.add(this.board.getCell(1, 0));
+        cells.add(this.board.getCell(1, 1));
+        this.region = new Region(cells);
+        region.setTotal(2);
+        this.board.addRegion(region);
+        this.rule = new RegionTotalConnectionRule();
+    }
+
+    @Test
+    public void checkEmptyBoard_ReturnFalse() {
+        Assert.assertTrue(!this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkwithOneCellOcuppiedWithMoreThanOneValueBoard_ReturnFalse() {
+        this.board.setValue(0, 0, new ValueContent(2));
+        this.board.setValue(0, 0, new ValueContent(2));
+        Assert.assertTrue(!this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkwithTwoCellOcuppied_ReturnTrue() {
+        this.board.setValue(0, 0, new ValueContent(2));
+        this.board.setValue(1, 0, new ValueContent(2));
+        Assert.assertTrue(this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkwithTwoCellsWithMoreThanOneValue_ReturnTrue() {
+        this.board.setValue(0, 0, new ValueContent(2));
+        this.board.setValue(1, 0, new ValueContent(2));
+        this.board.setValue(1, 0, new ValueContent(3));
+        Assert.assertTrue(this.rule.validate(this.board));
+    }
+
+
+    @Test
+    public void checkwithThreeCells_ReturnFalse() {
+        this.board.setValue(0, 0, new ValueContent(2));
+        this.board.setValue(1, 0, new ValueContent(2));
+        this.board.setValue(1, 1, new ValueContent(3));
+        Assert.assertTrue(!this.rule.validate(this.board));
+    }
+
+    @Test
+    public void checkWithoutRestrictions_ReturnTrue() {
+        this.board.setValue(0, 0, new ValueContent(2));
+        this.board.setValue(1, 0, new ValueContent(2));
+        this.board.setValue(1, 1, new ValueContent(3));
+        this.region.setTotal(-1);
+        Assert.assertTrue(this.rule.validate(this.board));
+    }
+}

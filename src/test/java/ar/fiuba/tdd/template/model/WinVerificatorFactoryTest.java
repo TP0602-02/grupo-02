a91307f3f6a7ac1bc475b1527c2a233ba71ee3b2
alealazraqui <alejandro.alazraqui@gmail.com>
2016-10-16 22:53:35 -0300

    agrego win verificator factory

---
+++ src/test/java/ar/fiuba/tdd/template/model/WinVerificatorFactoryTest.java
@@ -0,0 +1,29 @@
+package ar.fiuba.tdd.template.model;
+
+import ar.fiuba.tdd.template.winverificators.CloseCircuitVerificator;
+import ar.fiuba.tdd.template.winverificators.FullBoardWinVerificator;
+import ar.fiuba.tdd.template.winverificators.WinVerificator;
+import ar.fiuba.tdd.template.winverificators.WinVerificatorFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by alazraqui on 16/10/2016.
+ */
+public class WinVerificatorFactoryTest {
+
+    @Test
+    public void testCreateFullBoardVerificator_ReturnTrue() {
+        WinVerificator rule =
+                WinVerificatorFactory.getFactory().createVerificator(WinVerificatorFactory.FULL_BOARD_WIN_VERIFICATOR);
+        Assert.assertTrue(rule instanceof FullBoardWinVerificator);
+    }
+
+    @Test
+    public void testCreateCloseCircuitVerificator_ReturnTrue() {
+        WinVerificator rule =
+                WinVerificatorFactory.getFactory().createVerificator(WinVerificatorFactory.CLOSE_CIRCUIT_WIN_VERIFICATOR);
+        Assert.assertTrue(rule instanceof CloseCircuitVerificator);
+    }
+
+}

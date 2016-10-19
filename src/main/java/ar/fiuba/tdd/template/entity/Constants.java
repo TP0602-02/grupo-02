21b5baf73cf63eb63c88ef130d72b38a7c939e50
matikamien_93@hotmail.com <matikamien_93@hotmail.com>
2016-10-19 00:39:03 -0300

    Add circuit verificator for gokigen game

--- src/main/java/ar/fiuba/tdd/template/entity/Constants.java
+++ src/main/java/ar/fiuba/tdd/template/entity/Constants.java
@@ -14,6 +14,15 @@ public abstract class Constants {
     public static final int RIGHT_VALUE = 2;
     public static final int DOWN_VALUE = 4;
     public static final int LEFT_VALUE = 1;
+    public static final int UP_LEFT_VALUE = 5;
+    public static final int UP_RIGHT_VALUE = 6;
+    public static final int DOWN_LEFT_VALUE = 7;
+    public static final int DOWN_RIGHT_VALUE = 8;
+
+    public static final int UPPER_LEFT_CORNER = 1;
+    public static final int UPPER_RIGHT_CORNER = 2;
+    public static final int LOWER_LEFT_CORNER = 3;
+    public static final int LOWER_RIGHT_CORNER = 4;
 
     public static final int NO_CLUE_RESTRICTION = -1;
 }

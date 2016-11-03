5ea1c9b75657c0a2a6e6acf11e47b7dd6e558e1e
matikamien_93@hotmail.com <matikamien_93@hotmail.com>
2016-11-03 18:24:29 -0300

    Add SameClueConnection Rule to numberlink game

---
+++ src/main/java/ar/fiuba/tdd/template/rules/SameClueConnectionRule.java
@@ -0,0 +1,99 @@
+package ar.fiuba.tdd.template.rules;
+
+import ar.fiuba.tdd.template.board.Board;
+import ar.fiuba.tdd.template.board.cell.model.Cell;
+import ar.fiuba.tdd.template.board.cell.model.CellContent;
+import ar.fiuba.tdd.template.board.cell.model.ValueContent;
+import ar.fiuba.tdd.template.circuitverificator.BoardIteratorConnections;
+
+/**
+ * Created by matiaskamien on 02/11/16.
+ */
+public class SameClueConnectionRule extends GenericRule {
+    private BoardIteratorConnections iterator = new BoardIteratorConnections();
+    private int clueValue = -1;
+
+    @Override
+    public boolean validate(Board board, Cell cell, int numberToAdd) {
+        Cell nextCell = this.iterator.getNextCell(board, cell, numberToAdd);
+        Cell cellWithClue = this.getCellWithClue(cell, nextCell);
+        if (cellWithClue == null) {
+            return true;
+        }
+        this.generatePlay(cell, numberToAdd, nextCell);
+        this.clueValue = this.getClueValue(cellWithClue);
+        if (this.validConnection(board, cellWithClue, this.getPreviousDirection(cellWithClue))) {
+            this.removePlay(cell, nextCell);
+            return true;
+        }
+        this.removePlay(cell, nextCell);
+        return false;
+    }
+
+    private int getPreviousDirection(Cell cellWithClue) {
+        for (CellContent content: cellWithClue.getContents()) {
+            if (content.isSummable()) {
+                return content.getNumberValue();
+            }
+        }
+        return -1;
+    }
+
+    private void generatePlay(Cell cell, int numberToAdd, Cell nextCell) {
+        cell.getContents().add(new ValueContent(numberToAdd));
+        int nextCellValue = this.iterator.getOppositeDirection(numberToAdd);
+        nextCell.getContents().add(new ValueContent( nextCellValue ));
+    }
+
+    private void removePlay(Cell cell, Cell nextCell) {
+        int lastContentIndex = cell.getContents().size() - 1;
+        cell.getContents().remove( lastContentIndex );
+        lastContentIndex = nextCell.getContents().size() - 1;
+        nextCell.getContents().remove( lastContentIndex );
+    }
+
+    private boolean validConnection(Board board, Cell cell, int previousDirection) {
+        Cell nextCell = this.iterator.getNextCell(board, cell, previousDirection);
+        if (this.hasClue(nextCell) && this.isSameValue(nextCell)) {
+            return true;
+        } else if (this.hasClue(nextCell) && !this.isSameValue(nextCell)) {
+            return false;
+        }
+        return this.validConnection(board, nextCell, this.getOtherValue(nextCell,this.iterator.getOppositeDirection(previousDirection)));
+    }
+
+    private boolean isSameValue(Cell cell) {
+        return this.clueValue == this.getClueValue(cell);
+    }
+
+    private int getOtherValue(Cell nextCell, int previousValue) {
+        for (CellContent content: nextCell.getContents()) {
+            if (content.getNumberValue() != previousValue) {
+                return content.getNumberValue();
+            }
+        }
+        return -1;
+    }
+
+    private int getClueValue(Cell cell) {
+        for (CellContent content: cell.getContents()) {
+            if (!content.isSummable()) {
+                return content.getNumberValue();
+            }
+        }
+        return -1;
+    }
+
+    private Cell getCellWithClue(Cell cell, Cell nextCell) {
+        if (this.hasClue(cell)) {
+            return cell;
+        } else if (this.hasClue(nextCell)) {
+            return nextCell;
+        }
+        return null;
+    }
+
+    private boolean hasClue(Cell cell) {
+        return (cell.getContents().size() > cell.getSummableContents().size());
+    }
+}
---
+++ src/test/java/ar/fiuba/tdd/template/model/SameClueConnectionRuleTest.java
@@ -0,0 +1,101 @@
+package ar.fiuba.tdd.template.model;
+
+import ar.fiuba.tdd.template.board.Board;
+import ar.fiuba.tdd.template.board.cell.model.Cell;
+import ar.fiuba.tdd.template.board.cell.model.ClueContent;
+import ar.fiuba.tdd.template.board.cell.model.ValueContent;
+import ar.fiuba.tdd.template.entity.Coordinate;
+import ar.fiuba.tdd.template.rules.SameClueConnectionRule;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by matiaskamien on 02/11/16.
+ */
+public class SameClueConnectionRuleTest {
+    public static final int LEFT = 1;
+    public static final int RIGHT = 2;
+    public static final int UP = 3;
+    public static final int DOWN = 4;
+    private Board board;
+    private SameClueConnectionRule rule;
+
+    @Before
+    public void setUp() {
+        this.board = new Board(4, 4, "");
+        this.rule = new SameClueConnectionRule();
+    }
+
+    @Test
+    public void addValueInNormalCellWithSameClue_ReturnTrue() {
+        Cell clueCell = this.board.getCell(new Coordinate(0,1));
+        clueCell.getContents().add(new ClueContent(2));
+
+        Cell otherClueCell = this.board.getCell(new Coordinate(3,1));
+        otherClueCell.getContents().add(new ClueContent(2));
+        otherClueCell.getContents().add(new ValueContent(UP));
+
+        Cell firstCell = this.board.getCell(new Coordinate(2,1));
+        firstCell.getContents().add(new ValueContent(DOWN));
+        firstCell.getContents().add(new ValueContent(UP));
+
+        Cell secondCell = this.board.getCell(new Coordinate(1,1));
+        secondCell.getContents().add(new ValueContent(DOWN));
+        Assert.assertTrue(this.rule.validate(board, secondCell, UP));
+    }
+
+    @Test
+    public void addValueInNormalCellWithDifferentClue_ReturnFalse() {
+        Cell clueCell = this.board.getCell(new Coordinate(0,1));
+        clueCell.getContents().add(new ClueContent(2));
+
+        Cell otherClueCell = this.board.getCell(new Coordinate(3,1));
+        otherClueCell.getContents().add(new ClueContent(3));
+        otherClueCell.getContents().add(new ValueContent(UP));
+
+        Cell firstCell = this.board.getCell(new Coordinate(2,1));
+        firstCell.getContents().add(new ValueContent(DOWN));
+        firstCell.getContents().add(new ValueContent(UP));
+
+        Cell secondCell = this.board.getCell(new Coordinate(1,1));
+        secondCell.getContents().add(new ValueContent(DOWN));
+        Assert.assertTrue(!this.rule.validate(board, secondCell, UP));
+    }
+
+    @Test
+    public void addValueInClueCellWithSameClue_ReturnTrue() {
+        Cell clueCell = this.board.getCell(new Coordinate(0,1));
+        clueCell.getContents().add(new ClueContent(2));
+
+        Cell otherClueCell = this.board.getCell(new Coordinate(3,1));
+        otherClueCell.getContents().add(new ClueContent(2));
+        otherClueCell.getContents().add(new ValueContent(UP));
+
+        Cell firstCell = this.board.getCell(new Coordinate(2,1));
+        firstCell.getContents().add(new ValueContent(DOWN));
+        firstCell.getContents().add(new ValueContent(UP));
+
+        Cell secondCell = this.board.getCell(new Coordinate(1,1));
+        secondCell.getContents().add(new ValueContent(DOWN));
+        Assert.assertTrue(this.rule.validate(board, clueCell, DOWN));
+    }
+
+    @Test
+    public void addValueInClueCellWithDifferentClue_ReturnFalse() {
+        Cell clueCell = this.board.getCell(new Coordinate(0,1));
+        clueCell.getContents().add(new ClueContent(2));
+
+        Cell otherClueCell = this.board.getCell(new Coordinate(3,1));
+        otherClueCell.getContents().add(new ClueContent(3));
+        otherClueCell.getContents().add(new ValueContent(UP));
+
+        Cell firstCell = this.board.getCell(new Coordinate(2,1));
+        firstCell.getContents().add(new ValueContent(DOWN));
+        firstCell.getContents().add(new ValueContent(UP));
+
+        Cell secondCell = this.board.getCell(new Coordinate(1,1));
+        secondCell.getContents().add(new ValueContent(DOWN));
+        Assert.assertTrue(!this.rule.validate(board, clueCell, DOWN));
+    }
+}
